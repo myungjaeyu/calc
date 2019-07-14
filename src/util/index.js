c4bbb6e1ce625cf1b5132232900de2142b8f2fde
u4bi <rootcode10@gmail.com>
2019-07-14 13:13:32 +0900

    toComma 유틸 함수 추가

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,16 @@
 export const isNumber = text => !isNaN(text)
 
 export const calculation = (result, operation, value) => eval(`${ result }${ operation }${ value }`)
+
+export const toComma = (text, digit) => {
+
+    const arr = text.toString().split('.')
+
+    const integer = arr[0]
+    const decimal = arr[1] ? `.${ arr[1].slice(0, digit) }` : ''
+
+    const result = integer.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + decimal
+
+    return result
+
+}
